66b32034dce74e84be6fd77f478f8af6f81e7f3c
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-11 14:12:54 -0700

    search iDfs

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,30 +1,67 @@
 import { UGraphNodeStr } from "../graph/graph";
+import { Stack } from "../common/stack";
 
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
-    start: UGraphNodeStr,
-    result: UGraphNodeStr[] = [],
-    visited = new Set([start])): UGraphNodeStr[] {
+  start: UGraphNodeStr,
+  result: UGraphNodeStr[] = [],
+  visited = new Set([start])): UGraphNodeStr[] {
 
-      result = [start]
-      visited.add(start)
+  result = [start];
+  visited.add(start);
 
-      for(const node of start.adjacent){
-        if(!visited.has(node)){
-          result.push(...rDfs(node, result, visited))
-        }
-        // visited.add(node)
-      }
-      return result
+  for (const node of start.adjacent) {
+    if (!visited.has(node)) {
+      result.push(...rDfs(node, result, visited));
+    }
+  }
+  return result;
 }
 
 // d -> b
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
+/*
+input: node
+output: array of nodes
+
+check each node's adjacent Set
+- for each adj Node, check if it's been visited
+- if yes:
+  - skip it
+- if not:
+  - add it to stack to visit
+  - repeat above
+
+
+
+  //push pop
+  d -- b -- a -- c
+
+  c
+  a
+  b
+  d
+*/
+
 function iDfs(start: UGraphNodeStr): UGraphNodeStr[] {
-  return ["todo"];
+  const toVisit = new Stack<UGraphNodeStr>([start]);
+  const visited = new Set([start]);
+
+  while (!toVisit.isEmpty()) {
+    const currNode = toVisit.pop();
+    for (let node of currNode.adjacent) {
+      if (!visited.has(node)) {
+        toVisit.push(node);
+        visited.add(node);
+      }
+    }
+  }
+
+  return Array.from(visited);
+
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
